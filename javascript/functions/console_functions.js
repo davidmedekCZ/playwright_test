2ce6450d8814f34258f50e2805b9e8320f53b44e
David Medek <d4vid.medek@gmail.com>
2025-11-05 17:31:37 +0100

    arrays,forEach,objects

---
+++ javascript/functions/console_functions.js
@@ -0,0 +1,12 @@
+function logHelloWorld() {
+  console.log("Hello world!");
+}
+
+function logText(textParameter) {
+  console.log("2025-11-05 15:45 - " + textParameter);
+}
+
+logHelloWorld();
+logHelloWorld();
+logText("Jsme mistři JS!");
+logText("Těšíme se na objekty!");
---
+++ objects/cars.js
@@ -0,0 +1,7 @@
+import { CarBlueprint } from "./car_blueprint.js";
+
+const dieselCar = new CarBlueprint("Červená", "1.6TDI", "Diesel", "Ropák");
+dieselCar.logCarProperties();
+
+const electricCar = new CarBlueprint("Modrá", "EV 160kW", "Elektřina", "Blesk");
+electricCar.logCarProperties();
