098ecf08829bb08228a2aa44f9f3d91e1e3e9f18
David Medek <d4vid.medek@gmail.com>
2025-11-08 00:14:59 +0100

    hw_calculator

---
+++ hw_calculator.js
@@ -0,0 +1,24 @@
+const add = (a, b) => {
+  const result = a + b;
+  console.log(`${a} + ${b} = ${result}`);
+};
+
+const subtract = (a, b) => {
+  const result = a - b;
+  console.log(`${a} - ${b} = ${result}`);
+};
+
+const multiply = (a, b) => {
+  const result = a * b;
+  console.log(`${a} * ${b} = ${result}`);
+};
+
+const divide = (a, b) => {
+  const result = a / b;
+  console.log(`${a} / ${b} = ${result}`);
+};
+
+add(565, 584);
+subtract(474, 520);
+multiply(5822, 65);
+divide(632, 6);
